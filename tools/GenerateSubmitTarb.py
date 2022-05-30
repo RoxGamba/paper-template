32ecc3ef5ec9c7de091c4cb87600cfc1a2d145b3
Rossella <rossella.gamba@ligo.org>
2022-05-30 18:10:35 +0200

    add tools for arxiv sub

---
+++ tools/GenerateSubmitTarb.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+"""
+Generate tarball for submission to arXiv.
+
+Example:
+$ ./GenerateSubmitTarb.py --tex letter.tex supplmat.tex --bbl letter.bbl
+
+rgamba, 05/06/21
+"""
+
+import os, datetime, string, argparse, tarfile
+from shutil import copyfile
+
+def find_pattern(lines, pattern):
+    return [line.strip('\n') for line in lines if pattern in line]
+
+# taken from 
+# https://stackoverflow.com/questions/2032403/how-to-create-full-compressed-tar-file-using-python
+def make_tarfile(output_filename, source_dir):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--tex',    nargs='*', help='.tex files for the submission (required, at least one)', required=True)
+parser.add_argument('--bbl',    help='bbl file (required)', required=True)
+parser.add_argument('--outdir', help='path to output directory (optional)')
+args = parser.parse_args()
+
+# path of the submission dir -- if not user-input, use ddmmyyyy
+dirpath = args.outdir
+if dirpath == None:
+    now = datetime.datetime.now() 
+    dirpath = 'submission'+str(now.day)+str(now.month)+str(now.year)
+
+print("Directory name:", dirpath)
+
+# create dir
+try:
+    os.mkdir(dirpath)
+except FileExistsError:
+    print("WARNING: Dir already exists, over-writing")
+
+# fix .tex files given in
+# remove comments, change figure names
+
+# patterns for figs, comments, bbl
+incl_gr = "\includegraphics["
+beg_fig = r"\begin{figure"
+end_fig = r"\begin{figure"
+comment = "%% "
+biblio  = r"\bibliography"
+
+abcs     = list(string.ascii_lowercase)
+
+# copy the .bbl file to the new dir
+print("* Copying .bbl file", args.bbl)
+copyfile(args.bbl, dirpath+'/'+args.bbl)
+
+# read in old file and find the patterns
+for tx in args.tex:
+    print("* Processing file", tx)
+    with open(tx, 'r') as t:
+        # all lines
+        lns      = t.readlines()
+        # find lines starting with %%
+        lns_comm = find_pattern(lns, comment)
+        # find figure lines
+        lns_figs = find_pattern(lns, incl_gr)
+        # find beginfigures
+        lns_begf = find_pattern(lns, beg_fig)
+        # find endfigures
+        lns_endf = find_pattern(lns, end_fig)
+        # find biblio
+        lns_bibl = find_pattern(lns, biblio)
+
+    # create the new .tex in dir
+    texnew = dirpath+'/'+tx
+
+    figN     = 0
+    subpltN  = 0
+
+    with open(texnew, 'w') as tnew:
+        for line in lns:
+
+            # \begin{figure} found, increment fig Number
+            if line.strip('\n') in lns_begf: 
+                figN = figN+1
+
+            # \end{figure} found, restart counting subplots
+            if line.strip('\n') in lns_endf:
+                subpltN = 0
+
+            # subplot found
+            if line.strip('\n') in lns_figs:
+                
+                # find path and extension of figure
+                figpath    = line.strip('\n').split("{")[1].split("}")[0]
+                figext     = figpath.split('.')[-1]
+                
+                # rename it (based on name of .tex, figN and subpltN)
+                nm         = tx.split('.')[0]
+                newfigpath = 'fig'+nm+str(figN)+abcs[subpltN]+'.'+figext
+                subpltN    = subpltN+1
+                print("\t", figpath, "-->", dirpath+'/'+newfigpath)
+
+                # copy figure to dir
+                copyfile(figpath, dirpath+'/'+newfigpath)
+                
+                # replace figpath with newfig in line
+                line = line.replace(figpath, newfigpath)
+
+            # bibliography
+            if line.strip('\n') in lns_bibl:
+                line = r'\input{%s}' %args.bbl
+
+            # remove comments at the end
+            if line.strip('\n') in lns_comm:
+                line = line.split(comment)[0]
+
+            # write the line
+            tnew.write(line) 
+
+# tar the dir
+make_tarfile(dirpath+'.tar.gz', dirpath)
---
+++ tools/arxiv_submit.py
@@ -0,0 +1,87 @@
+"""
+Submit a paper to the arxiv. Requires:
+- selenium library (pip install selenium)
+- chromium-chromedriver (apt install chromium-chromedriver)
+- schedule library
+
+See also:
+https://stackoverflow.com/questions/33155454/how-to-find-an-element-by-href-value-using-selenium-python/33155512
+https://towardsdatascience.com/controlling-the-web-with-python-6fceb22c5f08
+"""
+
+from selenium import webdriver
+
+def create_webdriver():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    # Using Chrome to access web
+    return webdriver.Chrome('chromedriver',options=options) 
+
+def arxiv_login(wd, usr, pwd, hrf):
+    from selenium.webdriver.common.by import By
+    # Open the website
+    wd.get('https://arxiv.org/')
+    # find the login button
+    login_button = wd.find_element(By.CLASS_NAME,'login')
+    # click it
+    login_button.click()
+
+    # Send id information
+    usrname = wd.find_element(By.NAME, "username")
+    usrname.send_keys(usr)
+
+    # Send pwd information
+    pwd     = wd.find_element(By.NAME, "password")
+    pwd.send_keys(pwd)
+
+    # login
+    submit_button = wd.find_element(By.XPATH,'//input[@value="Submit"]')
+    submit_button.click()
+
+    # find the correct paper to update/submit via href
+    # eg: https://arxiv.org/submit/4013143/resume --> hrf = '4013143'
+    update_button = wd.find_element(By.XPATH,('//a[@href="https://arxiv.org/submit/' + hrf + '/resume"]'))
+    update_button.click()
+
+    return 0
+
+def arxiv_submit(usr, pwd, hrf, time="18:00:00"):
+    from   selenium.webdriver.common.by import By
+    import schedule; import time
+    
+    # create a chrome session
+    wd = create_webdriver()
+
+    # open and login to the arxiv, go to the paper update page
+    arxiv_login(wd, usr, pwd, hrf)
+
+    # find the submit button
+    submit_button = wd.find_element(By.XPATH,'//input[@value="Submit"]')
+
+    # local function to click submit
+    def click_submit():
+        submit_button.click()
+        return schedule.CancelJob
+
+    # scheduler: run it at specified UTC time
+    schedule.every().day.at(time).do(click_submit)
+
+
+if __name__ == '__main__':
+
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-p', action='store', required=True, type=str, help='your arxiv password')
+    parser.add_argument('-u', action='store', required=True, type=str, help='your arxiv username')
+    parser.add_argument('-href', action='store', required=True, type=str, help='your arxiv href.\n E.g: https://arxiv.org/submit/4013143/resume --> href = 4013143')
+
+
+    args = parser.parse_args()
+
+    arxiv_submit(args.u, args.p, args.href)
+
+
